10be006052f5bc5b01f1a7f6925f294c9e7c3300
OppKunG <67791331+OppKunG@users.noreply.github.com>
2023-05-12 13:28:55 +0700

    Test C#

---
+++ Test C#/app/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1000, b = 2000, ans = a + b;
+string hello = "Hello Welcome! ";
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+Console.Write(hello + ans + "\nEnter username: ");
+
+string userName = Console.ReadLine();
+
+Console.Write("Hi " + userName + "\nWhat brand of car do you like?\n");
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.Write(cars[i]);
+    if (i != cars.Length - 1) Console.Write(", ");
+    else continue;
+}
+
+//Run: dotnet run
